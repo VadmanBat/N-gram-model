bdd8ed660dff0b1e6323103d532f87beedcd66c2
Vadim Eliseev <38383179+VadmanBat@users.noreply.github.com>
2022-09-11 10:46:05 +0300

    Add files via upload

---
+++ train.py
@@ -0,0 +1,45 @@
+import os
+import re
+import pickle
+import argparse
+
+
+class Train:
+    words = []
+    words_num, links = {}, {}
+
+    def fit(self, file_name):
+        text = open(args.input + '/' + file_name, encoding='utf-8').read().lower()
+        text = re.sub('[^a-zа-яё]', ' ', text).split()
+        for word in text:
+            if word in self.words_num:
+                continue
+            self.words_num[word] = len(self.words)
+            self.words.append(word)
+        for i in range(1, len(text)):
+            key = self.words_num[text[i - 1]]
+            self.links.setdefault(key, []).append(self.words_num[text[i]])
+        for i in range(2, len(text)):
+            key = (self.words_num[text[i - 2]], self.words_num[text[i - 1]])
+            self.links.setdefault(key, []).append(self.words_num[text[i]])
+
+    def read_files(self):
+        files = os.listdir(args.input)
+        for file in files:
+            self.fit(file)
+
+    def dump_model(self):
+        model = open(args.model, 'wb')
+        pickle.dump(self.words, model)
+        pickle.dump(self.words_num, model)
+        pickle.dump(self.links, model)
+
+
+parser = argparse.ArgumentParser(description="Train Model for Text Generation")
+parser.add_argument("-d", "--input", type=str, metavar='', help="path to the directory containing the collection of documents")
+parser.add_argument("-m", "--model", type=str, metavar='', required=True, help="path to the file where the model is saved")
+args = parser.parse_args()
+
+train = Train()
+train.read_files()
+train.dump_model()
